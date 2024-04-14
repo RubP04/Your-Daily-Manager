16b544efe761d7963c65124e10a3f73715414050
RubP04 <158429989+RubP04@users.noreply.github.com>
2024-04-14 16:59:36 -0400

    functional still but before firebase implementations

---
+++ firebase.js
@@ -0,0 +1,18 @@
+//Firebase Configuration Information
+const firebaseConfig = {
+    apiKey: "AIzaSyDwMJ7P-M6V0G9WuNAF-wi2kqC_hYfQU7A",
+    authDomain: "your-daily-manager.firebaseapp.com",
+    databaseURL: "https://your-daily-manager-default-rtdb.firebaseio.com",
+    projectId: "your-daily-manager",
+    storageBucket: "your-daily-manager.appspot.com",
+    messagingSenderId: "1005772940945",
+    appId: "1:1005772940945:web:5eae59eeb55036865b0130",
+    measurementId: "G-DVZGNW7B05"
+  };
+
+//Firebase Initilization
+firebase.initializeApp(firebaseConfig);
+
+//Firebase Variables for Firebase Funcitons
+const auth = firebase.auth()
+const database = firebase.database()
